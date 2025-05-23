452017590b65a85e82be7fafc1a7bb8391eb0230
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-05-23 19:43:25 +0200

    feat(namespace): create argocd namespace

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -22,3 +22,8 @@ usermod -aG docker vagrant
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+
+#Kubectl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
