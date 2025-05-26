b7c5abdfbe97758f9e046dfe42646bfda869611c
Florent Gacougnolle <fgacougn@f6r4s15.clusters.42paris.fr>
2025-05-26 11:31:50 +0200

    ajout argocd

--- srcs/p3/scripts/dependancies.sh
+++ srcs/p3/scripts/dependancies.sh
@@ -1,4 +1,6 @@
 pt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-kubectl create namespace argo-nm
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl create namespace dev
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
