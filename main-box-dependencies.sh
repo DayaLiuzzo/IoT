0a29a893ae7696f0dcbfbff73f931a080979c410
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-05-30 21:16:13 +0200

    feat(scripts) ADD: Init script for argocd lab

--- main-box-dependencies.sh
+++ main-box-dependencies.sh
@@ -3,7 +3,7 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt install vagrant
 
-
+sudo apt install make
 
 echo "Installing VirtualBox dependencies..."
 # Install VirtualBox and dependencies
--- srcs/p3/scripts/init.sh
+++ srcs/p3/scripts/init.sh
@@ -4,6 +4,7 @@ k3d cluster create --config conf/k3d-argocd-lab.yaml
 #argocd install
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl apply -n argocd -f conf/install.yaml
 
 #argocd cli
 VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)
@@ -13,3 +14,13 @@ rm argocd-linux-amd64
 
 #dev namespace
 kubectl create namespace dev
+
+
+#argo cd set up
+
+# kubectl apply -f conf/ingress.yaml -n argocd
+
+
+# kubectl patch deployment argocd-server -n argocd \
+#   --type=json \
+#   -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--insecure"}]'
