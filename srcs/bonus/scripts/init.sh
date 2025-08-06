51d0ba80223e3d3a8896e6c3a12c4d8eb091906c
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-08-06 20:04:22 +0200

    feat : add unfinished argocd sync

--- srcs/bonus/scripts/init.sh
+++
@@ -1,6 +0,0 @@
-#init cluster
-k3d cluster create argocd-lab
-
-#dev namespace
-kubectl create namespace dev
-
--- srcs/bonus/scripts/update_repo.sh
+++ srcs/bonus/scripts/update_repo.sh
@@ -1,13 +1,26 @@
-# MANUALLY CREATE THE REPO IN GITLAB UI BEFORE LAUNCHING THIS
+#!/bin/bash
 
-
+# Get GitLab root password
 GITLAB_PASS=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
 
-# Write the .netrc file directly to /root/
-sudo bash -c "echo -e 'machine gitlab.k3d.gitlab.com:8082\nlogin root\npassword ${GITLAB_PASS}' > /root/.netrc"
+# Write .netrc for current user (adjust if needed for root)
+echo -e "machine gitlab.k3d.gitlab.com:8082\nlogin root\npassword ${GITLAB_PASS}" > ~/.netrc
+chmod 600 ~/.netrc
 
-# Set strict permissions
-sudo chmod 600 /root/.netrc
-
-# clone the gitlab repo
+# Clone GitLab and GitHub repos
 git clone http://gitlab.k3d.gitlab.com:8082/root/simple-gitlab-app.git gitlab_repo
+git clone https://github.com/DayaLiuzzo/dliuzzo_iot github_repo
+
+# Copy GitHub contents into GitLab repo
+cp -r github_repo/* gitlab_repo/
+rm -rf github_repo
+
+# Commit and push changes
+cd gitlab_repo
+git add .
+git commit -m "Update from GitHub repo"
+git push
+cd ..
+
+# Apply ArgoCD application
+kubectl apply -f ../conf/argocd-deploy.yaml
